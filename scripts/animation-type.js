5c4d6ba324783727b5d3634ef3083b08bcead43d
yThiagoFS <99218990+yThiagoFS@users.noreply.github.com>
2022-04-10 21:40:09 -0300

    type writing

---
+++ scripts/animation-type.js
@@ -0,0 +1,43 @@
+
+
+function typeWrite(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(function () {
+            elemento.innerHTML += letra;
+        }, 50 * i)
+    })
+}
+
+const titulo = document.getElementById('titulo1')
+typeWrite(titulo);
+
+function typeWrite2(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(function () {
+            elemento.innerHTML += letra;
+        }, 50 * i)
+    })
+}
+
+const titulo2 = document.getElementById('titulo2')
+typeWrite2(titulo2);
+
+
+
+
+function typeWrite3(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(function () {
+            elemento.innerHTML += letra;
+        }, 50 * i)
+    })
+}
+
+const titulo3 = document.getElementById('titulo3')
+typeWrite3(titulo3);
